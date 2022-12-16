81a39cb3674e219a884b17160c09ca4f0d5afdd5
prooni <lhw629@naver.com>
2022-12-16 11:43:58 +0900

    Add files via upload

---
+++ Multicast_Send_Receive/Multicast/Multicast/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Text;
+
+namespace Multicast
+{
+    //Sender
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // UdpClient 객체 생성
+            UdpClient udpClient = new UdpClient();
+
+            // IP 주소 설정
+            IPAddress multicastaddress = IPAddress.Parse("239.0.0.222");
+            udpClient.JoinMulticastGroup(multicastaddress);
+
+            // Multicast 중단점 설정
+            IPEndPoint remoteEP = new IPEndPoint(multicastaddress, 2222);
+
+            byte[] buffer = null;
+
+            Console.WriteLine("메세지를 전송하려면 엔터키를 눌러주세요.");
+            Console.ReadLine();
+
+            for(int i = 0; i <= 10; i++)
+            {
+                buffer = Encoding.Unicode.GetBytes(i.ToString());
+
+                //Multicast 그룹에 데이터그램 전송
+                udpClient.Send(buffer, buffer.Length, remoteEP);
+                Console.WriteLine("Sent : {0}", i.ToString());
+                Thread.Sleep(1000);
+            }
+
+            buffer = Encoding.Unicode.GetBytes("quit");
+            udpClient.Send(buffer, buffer.Length, remoteEP);
+
+            udpClient.Close();
+
+            Console.WriteLine("All Done! Press ENTER to quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Multicast_Send_Receive/Multicast_Receiver/Multicast_Receiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Text;
+
+namespace Multicast_Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UdpClient 객체 생성
+            UdpClient client = new UdpClient();
+
+            //UDP 로컬 IP포트에 바인딩
+            client.ExclusiveAddressUse = false;
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.ExclusiveAddressUse = false;
+
+            client.Client.Bind(localEp);
+
+            //Multicast 그룹에 Join
+            IPAddress multicastaddress = IPAddress.Parse("239.0.0.222");
+            client.JoinMulticastGroup(multicastaddress);
+
+            Console.WriteLine("Listening this will quit");
+
+            while(true)
+            {
+                //Multicast 수신
+                byte[] data = client.Receive(ref localEp);
+                string strData = Encoding.Unicode.GetString(data);
+                Console.WriteLine("receied data : {0}", strData);
+
+                if (strData == "quit")
+                    break;
+            }
+
+            Console.WriteLine("quit the program to ENTER");
+            Console.ReadLine();
+        }
+    }
+}
